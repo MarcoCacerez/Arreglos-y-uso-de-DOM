d874b98763767df42a02f0cc5382c9df5f244308
Marco <L18390579@chetumal.tecnm.mx>
2021-11-27 18:40:20 -0500

    comentar el archivo main.js del MTS

--- matrizTriangularSuperior/main.js
+++ matrizTriangularSuperior/main.js
@@ -1,35 +1,39 @@
-let matrix = [[1, 2, 3],
-              [0, 5, 6],
-              [0, 0, 9]];
-
 function verificar(matrix,n)
 {
     let i, j;
+    //bandera que nos indica en caso de que no sea una mts
     let flag = false;
+    //recorremos todas las filas
     for (i = 1; i < n; i++)
     {
+        //recorremos todas las columnas
         for (j = 0; j < i; j++)
         {
+            //obtenemos el valor de una posicion especifica
             let valor = matrix[i][j];
+            //si es cero, pues vamos bien
             if (valor == 0){
                 flag = true;
+            //si es otro numero, ya no cumple la condicion y se pone falso
             }else{
                 flag = false;
+                break;
             }
         }
     }
     return flag;
 }            
 
-//console.log(upper(matrix,matrix.length));
-
+//recibimos el contenido del Textarea
 function crearArreglo() {
+    //almacenamos el textarea
     let texto = document.getElementById('datos');
+    //lo volvemos una matriz
     let arreglo = texto.value.split("\n");
     for(i in arreglo){
         arreglo[i] = arreglo[i].split(" ");
     }
-
+    //verificamos si es una matriz nxn
     let estado = true;
 
     for( i in arreglo)
@@ -37,11 +41,13 @@ function crearArreglo() {
             estado = true;
         else
             estado = false;
-
+    //verificamos si esta vacia
     if(texto.value == ""){
         estado = false
     }
+    //en caso de que sea valida, se procede a verificar
     if(estado){
+        //enviamos la matriz que generamos y su longitud
         let validacion = verificar(arreglo,arreglo.length);
         if(validacion)
             alert('Es una matriz triangular superior');
@@ -50,5 +56,4 @@ function crearArreglo() {
     }
     else
         alert('La matriz no es n x n');
-    //console.log(arreglo);
 }
